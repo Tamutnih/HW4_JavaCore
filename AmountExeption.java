37e89e17e44d3fd82da7656b2823b33c9e8ff980
Tamutnih <renu07@list.ru>
2023-07-02 19:09:15 +0500

    solution

---
+++ AmountExeption.java
@@ -0,0 +1,16 @@
+package main.java.com.example;
+
+/**
+ * Исключение, выбрасываемое при неверном количестве товара.
+ */
+public class AmountException extends Exception {
+
+    /**
+     * Конструктор класса AmountException.
+     *
+     * @param message сообщение об ошибке
+     */
+    public AmountException(String message) {
+        super(message);
+    }
+}
